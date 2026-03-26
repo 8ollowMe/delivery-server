d9287ccf562916a0716d24e94ba13892f46f910e
annieha <hajh5447@naver.com>
2026-03-26 22:57:10 +0900

    feat: EnableJpaAuditing added

--- src/main/java/com/followMe/delivery_server/DeliveryServerApplication.java
+++ src/main/java/com/followMe/delivery_server/DeliveryServerApplication.java
@@ -2,8 +2,10 @@ package com.followMe.delivery_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class DeliveryServerApplication {
 
   public static void main(String[] args) {
