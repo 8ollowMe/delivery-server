8a1837279493aec3abb724335c1e1c77d000e8b3
annieha <hajh5447@naver.com>
2026-03-26 11:33:38 +0900

    fix: for spotless

--- src/main/java/com/followMe/delivery_server/DeliveryServerApplication.java
+++ src/main/java/com/followMe/delivery_server/DeliveryServerApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DeliveryServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DeliveryServerApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(DeliveryServerApplication.class, args);
+  }
 }
