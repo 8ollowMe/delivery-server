905502ee2c94658cd938870e802e907a78dca305
annieha <hajh5447@naver.com>
2026-03-26 11:01:08 +0900

    feat: init

---
+++ src/main/java/com/followMe/delivery_server/DeliveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.followMe.delivery_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryServerApplication.class, args);
+	}
+
+}
