a7062534970a11c9cd918cfce10c6ab4cc1108c5
annieha <hajh5447@naver.com>
2026-03-27 14:22:07 +0900

    refactor: vo 형식 통일

--- src/main/java/com/followMe/delivery_server/delivery/domain/OrderId.java
+++ src/main/java/com/followMe/delivery_server/delivery/domain/OrderId.java
@@ -1,21 +1,21 @@
 package com.followMe.delivery_server.delivery.domain;
 
+import static lombok.AccessLevel.PROTECTED;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import java.util.UUID;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Getter
 @Embeddable
+@Getter
+@AllArgsConstructor(access = PROTECTED)
+@NoArgsConstructor(access = PROTECTED)
 public class OrderId {
   @Column(nullable = false, columnDefinition = "uuid")
   private UUID value;
-
-  private OrderId(UUID value) {
-    this.value = value;
-  }
-
-  public OrderId() {}
 
   public static OrderId of(UUID value) {
     return new OrderId(value);
