50898cbd2bef00a9caf64052e77058cfd87eae92
annieha <hajh5447@naver.com>
2026-03-27 17:13:25 +0900

    fix: HubNodeInfo 에 sequence 추가

--- src/main/java/com/followMe/delivery_server/client/dto/HubNodeInfo.java
+++ src/main/java/com/followMe/delivery_server/client/dto/HubNodeInfo.java
@@ -3,4 +3,4 @@ package com.followMe.delivery_server.client.dto;
 import com.followMe.delivery_server.delivery.domain.enums.NodeType;
 import java.util.UUID;
 
-public record HubNodeInfo(UUID id, NodeType type, String name) {}
+public record HubNodeInfo(UUID id, NodeType type, String name, int sequence) {}
