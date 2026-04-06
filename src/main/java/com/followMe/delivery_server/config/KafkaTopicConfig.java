977342817938d6abac547a8364d9c5c67578c818
annieha <hajh5447@naver.com>
2026-04-06 15:02:07 +0900

    chore: feign 테스트 용 설정 파일 추가

--- src/main/java/com/followMe/delivery_server/config/KafkaTopicConfig.java
+++ src/main/java/com/followMe/delivery_server/config/KafkaTopicConfig.java
@@ -3,9 +3,11 @@ package com.followMe.delivery_server.config;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
+@Profile("!feign-test")
 public class KafkaTopicConfig {
 
   @Bean
