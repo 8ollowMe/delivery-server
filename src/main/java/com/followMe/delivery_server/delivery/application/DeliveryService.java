ad0fac3ec5753b020c1af7579268060f18fe9ff6
annieha <hajh5447@naver.com>
2026-04-07 19:31:15 +0900

    feat: recipient 정보조회

--- src/main/java/com/followMe/delivery_server/delivery/application/DeliveryService.java
+++ src/main/java/com/followMe/delivery_server/delivery/application/DeliveryService.java
@@ -15,6 +15,8 @@ import com.followMe.delivery_server.delivery.domain.service.DeliveryPermissionCh
 import com.followMe.delivery_server.delivery.domain.service.DeliverySequenceUpdater;
 import com.followMe.delivery_server.delivery.domain.service.HubRouteInfo;
 import com.followMe.delivery_server.delivery.domain.service.OrderDeliveryAssigner;
+import com.followMe.delivery_server.delivery.infra.client.UserClient;
+import com.followMe.delivery_server.delivery.infra.client.dto.HubNodeInfo;
 import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
@@ -27,6 +29,7 @@ public class DeliveryService {
 
   private final DeliveryRepository deliveryRepository;
   private final HubRouteInfo hubRouteInfo;
+  private final UserClient userClient;
   private final DeliveryManagerAssigner assigner;
   private final DeliveryQueryPort deliveryQueryPort;
   private final DeliveryPermissionChecker permissionChecker;
@@ -36,8 +39,20 @@ public class DeliveryService {
 
   @Transactional
   public DeliveryResponse.DeliveryCreate createDelivery(DeliveryRequest.Create command) {
-    List<Node> nodes = hubRouteInfo.getRouteNodes(command.sourceHubId(), command.vendorId());
-    Delivery delivery = Delivery.create(command.orderId(), nodes);
+    List<HubNodeInfo> nodes = hubRouteInfo.getRouteNodes(command.sourceHubId(), command.vendorId());
+    String vendorAddress = nodes.getLast().address();
+
+    UserInfo recipient = userClient.getUserInfo(command.recipientId());
+
+    Delivery delivery =
+        Delivery.create(
+            command.orderId(),
+            command.sourceHubId(),
+            vendorAddress,
+            recipient.name(),
+            recipient.slackId(),
+            nodes);
+
     deliveryRepository.save(delivery);
     Shipment shipment = delivery.getShipments().getFirst();
     NodeType type = shipment.getTo().getType();
--- src/main/java/com/followMe/delivery_server/delivery/domain/service/HubRouteInfo.java
+++ src/main/java/com/followMe/delivery_server/delivery/domain/service/HubRouteInfo.java
@@ -1,9 +1,9 @@
 package com.followMe.delivery_server.delivery.domain.service;
 
-import com.followMe.delivery_server.delivery.domain.Node;
+import com.followMe.delivery_server.delivery.infra.client.dto.HubNodeInfo;
 import java.util.List;
 import java.util.UUID;
 
 public interface HubRouteInfo {
-  List<Node> getRouteNodes(UUID fromHubId, UUID toVendorId);
+  List<HubNodeInfo> getRouteNodes(UUID fromHubId, UUID toVendorId);
 }
